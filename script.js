03d201f942f5306d0f5bfab98f3860b245c00005
Koundinya G <121346044+Koundu@users.noreply.github.com>
2024-01-09 16:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+		var Charecters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmonpqrstuvwxyz0123456789"
+		let randomString = "";
+		for(let i=0;i<l;i++){
+			var rnum = Math.floor(Math.random()*Charecters.length);
+			randomString += Charecters.substring(rnum, rnum+1);
+		}
+	return randomString;
 }
 
 // Do not change the code below.
